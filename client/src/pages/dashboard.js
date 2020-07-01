4551462a5df45f4810bfe58a71c87a77a9ef4b60
David Garza <dgarza0413@gmail.com>
2020-07-01 16:57:08 -0500

    added react router and dashboard page started table component

---
+++ client/src/pages/dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import data from '../data/sample.json';
+
+const Dashboard = () => {
+    console.log(data)
+    return (
+        <div>
+            This is the dashboard
+        </div>
+    )
+}
+
+export default Dashboard
