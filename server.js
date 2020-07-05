408109dfa08f5893b09dfe39fefe5e292389da9c
David Garza <dgarza0413@gmail.com>
2020-07-05 17:23:07 -0500

    changes

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(function (req, res) {
-    // res.sendFile(path.join(__dirname, "client/build/index.html"));
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+    // res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // Start the API server
