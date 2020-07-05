7de4c65a6dd4274ce074ac0a83961854edafbde5
David Garza <dgarza0413@gmail.com>
2020-07-05 17:21:53 -0500

    changes to sendFile

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(function (req, res) {
     // res.sendFile(path.join(__dirname, "client/build/index.html"));
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // Start the API server
