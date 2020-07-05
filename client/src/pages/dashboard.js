2d589fb3b9526f374317cf579955343c3c0961c6
David Garza <dgarza0413@gmail.com>
2020-07-05 17:00:35 -0500

    clean up

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -4,11 +4,35 @@ import data from '../data/sample.json';
 import TableDisplay from '../components/Table';
 
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 const Dashboard = () => {
     return (
         <Container>
-            <TableDisplay payee={data} />
+            <Row>
+                <Col sm={8} className={'mb-4'}>
+                    <Card border="light" bg={'light'} className="shadow-sm h-100">
+                        <Card.Header>Paymerang</Card.Header>
+                        <Card.Body>
+                            <h3>Make a selection (lowercase and special char)</h3>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col sm={4} className={'mb-4'}>
+                    <Card border="light" bg={'light'} className="shadow-sm h-100">
+                        <Card.Header>Total Vendors</Card.Header>
+                        <Card.Body>
+                            <h1>{data.length}</h1>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col sm={12}>
+                    <TableDisplay payee={data} />
+                </Col>
+            </Row>
         </Container>
     )
 }
