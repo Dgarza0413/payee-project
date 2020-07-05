7571dafb8bda970bfc9868ce3a7e293912e982fa
David Garza <dgarza0413@gmail.com>
2020-07-05 17:18:36 -0500

    added compression

--- server.js
+++ server.js
@@ -14,10 +14,9 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
-
 app.use(function (req, res) {
     // res.sendFile(path.join(__dirname, "client/build/index.html"));
-    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 // Start the API server
